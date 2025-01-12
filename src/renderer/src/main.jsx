76de4740f701d50b0fcbb5486b5b21b4ccacc33c
Paul Arthur <paularthurjnr611@gmail.com>
2025-01-12 12:04:47 +0000

    cleaned up the main page and added some routes

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -1,11 +1,27 @@
-import './assets/main.css'
+import './styles/all.css'
+import './styles/index.css'
+import './styles/fonts.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, createRoutesFromChildren, Route, RouterProvider } from 'react-router'
+
+import Root from './root.jsx'
+import AppLayout from './app/layout.jsx'
+import Home from './app/home.jsx'
+import { ThemeProvider } from './contexts/theme.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromChildren([
+    <Route path="/" element={<Root />} />,
+    <Route element={<AppLayout />}>
+      <Route path="/home" element={<Home />} />,
+    </Route>
+  ])
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider>
+    <RouterProvider router={router} />
+  </ThemeProvider>
 )
