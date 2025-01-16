2ed85c3d84462822f0d1488586e65afca09375cf
Paul Arthur <paularthurjnr611@gmail.com>
2025-01-16 06:03:25 +0000

    inventory screen structured

--- src/renderer/src/app/inventory.jsx
+++ src/renderer/src/app/inventory.jsx
@@ -1,7 +1,70 @@
 import styles from '../styles/inventory.module.css'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Inventory() {
-  return <div>inventory</div>
+  // -------------- handle search --------------
+  const [isSearching, setIsSearching] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
+
+  function handleSearch(e) {
+    e.length > 0 ? setIsSearching(true) : setIsSearching(false)
+    setSearchTerm(e)
+  }
+
+  return (
+    <section className={styles.inventorySection}>
+      <div className={styles.operation}>
+        <div className={styles.searchBar}>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => handleSearch(e.target.value)}
+            onFocus={() => searchTerm.length > 0 && setIsSearching(true)}
+            onBlur={() => setIsSearching(false)}
+            name="search"
+            placeholder="Search..."
+          />
+          <i
+            onClick={() => isSearching && setSearchTerm('')}
+            className={isSearching ? 'far fa-times' : 'far fa-search'}
+          ></i>
+        </div>
+      </div>
+
+      <div className={styles.tableContainer}>
+        <table>
+          <colgroup>
+            <col />
+            <col />
+            <col />
+            <col />
+            <col />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Quantity</th>
+              <th>Price</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Paracetamol</td>
+              <td>2</td>
+              <td>&#8373; 100.00</td>
+              <td>
+                <i className="fal fa-pen"></i>
+              </td>
+              <td>
+                <i className="fal fa-trash-can"></i>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  )
 }
